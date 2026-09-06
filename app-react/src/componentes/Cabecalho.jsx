3226c0a732a8ddb9f7b87155d383c53e7b7a97ae
gleber-thinkpad <gleberphant@gmail.com>
2026-09-06 09:19:28 -0300

    implementei o estilo em css agora app esta com o estilo correto

--- app-react/src/componentes/Cabecalho.jsx
+++ app-react/src/componentes/Cabecalho.jsx
@@ -4,7 +4,7 @@ import PUCBRASAO from "../assets/images/pucpr-brasao-redondo.png";
 
 export function Cabecalho() {
   return (
-    <Navbar className="app-navbar" expand="lg">
+    <Navbar className="app-navbar" expand="lg" data-bs-theme="dark">
       <Container>
         <Navbar.Brand href="#home">
           <img height="50" src={PUCBRASAO}></img>
@@ -17,8 +17,8 @@ export function Cabecalho() {
             <Nav.Link href="#home">Home</Nav.Link>
 
             <NavDropdown title="Usuários" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Listar</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Criar</NavDropdown.Item>
+              <NavDropdown.Item href="#Listar">Listar</NavDropdown.Item>
+              <NavDropdown.Item href="#Criar">Criar</NavDropdown.Item>
             </NavDropdown>
 
             <Nav.Link href="#link">Sobre</Nav.Link>
--- app-react/src/paginas/Layout.Page.jsx
+++ app-react/src/paginas/Layout.Page.jsx
@@ -1,26 +1,28 @@
-import { Container, Card, Navbar, NavbarBrand, NavLink } from "react-bootstrap";
-import { Outlet } from "react-router-dom";
+//estilos
 import "../estilos/Layout.Page.css";
+
+//dependencias
+import { Container, Card } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+
+//meus componentes
 import { Cabecalho } from "../componentes/Cabecalho";
 import { Rodape } from "../componentes/Rodape";
 
 export default function LayoutPage() {
   return (
     <>
-      <Container fluid className="app-conteiner">
+      <Container fluid className="app-container">
+        {/** Cabeçalho da aplicação */}
         <Card className="app-card">
           <Card.Header className="app-header">
             <Cabecalho />
           </Card.Header>
-
+          {/** Corpo da aplicação */}
           <Card.Body className="app-body">
-            <Card bg="primary" className=" rounded-4">
-              <Card.Header>header</Card.Header>
-              <Card.Body>body</Card.Body>
-              <Card.Footer>footer</Card.Footer>
-            </Card>
             <Outlet />
           </Card.Body>
+          {/** Rodape da aplicação */}
           <Card.Footer className="app-footer">
             <Rodape />
           </Card.Footer>
